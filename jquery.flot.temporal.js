4bedf9970e5283c524c13013214706ee2cbca825
colinhunt <colin.hunt@live.com>
2016-12-12 14:54:21 -0700

    Extracted methods for clarity.

--- jquery.flot.temporal.js
+++ jquery.flot.temporal.js
@@ -5,7 +5,8 @@
 			timezone: null,		// "browser" for local to the client or timezone for timezone-js
 			timeformat: null,	// format string to use
 			twelveHourClock: false,	// 12 or 24 time in time mode
-			monthNames: null	// list of names of months
+			monthNames: null,	// list of names of months
+      tolerance: 0.7,
 		}
 	};
 
@@ -15,7 +16,7 @@
 
 	function multiplesOf(multiple, unit, t) {
 		let overflow = overflowValueOf[unit]
-		
+
 		let multiples = []
 		if (unit == "days") {
 			overflow = t.daysInMonth()
@@ -62,7 +63,16 @@
 		return start;
 	}
 
-	function init(plot) {
+  function log(multiple, unit, t, start, ticks) {
+    console.log(`multiple: ${multiple}`)
+    console.log(`unit: ${unit}`)
+    console.log(multiplesOf(multiple, unit, t))
+    console.log(`start: ${start.format()}`)
+    console.log(`t: ${t.format()}`)
+    console.log(`tick length: ${ticks.length}`)
+  }
+
+  function init(plot) {
 		// init durations & overflowValueOf
 		for (var i = 0; i < units.length - 1; i++) {
 			let maxValue = moment.duration(1, units[i + 1]).as(units[i])
@@ -73,10 +83,22 @@
 
 		// console.log(JSON.stringify(durations))
 
-		plot.hooks.processOptions.push(function (plot, options) {
-			$.each(plot.getAxes(), function(axisName, axis) {
-				const yearMultiples = [1, 2, 5]
+    function getMultiples(delta) {
+      let d = durations.find(d => {
+        return moment.duration(d).valueOf() > delta / 2
+      })
+      if (!d)
+        for (let i = 1; moment.duration(d).valueOf() <= delta / 2; i *= 10) {
+          for (const m of [1, 2, 5]) {
+            d = {"years": i * m}
+            if (moment.duration(d).valueOf() > delta / 2) break;
+          }
+        }
+      return d;
+    }
 
+    plot.hooks.processOptions.push(function (plot, options) {
+			$.each(plot.getAxes(), function(axisName, axis) {
 				let opts = axis.options;
 
 				if (opts.mode != "temporal")
@@ -86,21 +108,13 @@
 					let min = moment.tz(axis.min, opts.timezone);
 					let max = moment.tz(axis.max, opts.timezone);
 					let delta = Math.floor(axis.delta);
-
-					let d = durations.find(d => {return moment.duration(d).valueOf() > delta / 2})
-					if (!d)
-						for (let i = 1; moment.duration(d).valueOf() <= delta / 2; i*=10) {
-							for (const m of yearMultiples) {
-							 	d = {"years": i*m}
-								if (moment.duration(d).valueOf() > delta / 2) break;
-							}
-						}
+          var d = getMultiples(delta);
 
 					let unit = Object.keys(d)[0]
 					let multiple = d[unit]
 
 					axis.tickSize = [multiple, unit]
-					
+
 					// console.log(unit)
 					// console.log(multiple)
 					let start = getStartTick(min, unit, multiple)
@@ -110,17 +124,13 @@
 						// rotate through multiples, setting the unit value of t
 						// each iteration we get a new multiples array, last value will cause t to bubble up to the next higher unit
 						// (example: unit = hours, multiples = [0, 12, 24], we set hour to 0, then 12, then 24, which causes t to advance to the next day, then repeat)
-						console.log(`multiple: ${multiple}`)
-						console.log(`unit: ${unit}`)
-						console.log(multiplesOf(multiple, unit, t))
-						console.log(`start: ${start.format()}`)
-						console.log(`t: ${t.format()}`)
-						console.log(`tick length: ${ticks.length}`)
+            log(multiple, unit, t, start, ticks);
+
 						for (const m of multiplesOf(multiple, unit, t)) {
 							increment(t, unit, m)
 							let space = t.diff(ticks[ticks.length - 1], unit)
-						
-							if (space < multiple * 0.7)
+
+							if (space < multiple * opts.tolerance)
 								ticks.pop()
 
 							ticks.push(t.clone())
