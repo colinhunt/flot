372e4296d9080849bfd911d1236f1370ddcfd89b
colinhunt <colin.hunt@live.com>
2016-12-12 23:22:58 -0700

    Refactoring for format and legibility.

--- jquery.flot.temporal.js
+++ jquery.flot.temporal.js
@@ -18,7 +18,7 @@
             return factorsOf(maxValueOfUnit(i)).map(f => {
                 return {[u]: f}
             })
-        }).reduce((a, l) => a.concat(l), [] );
+        }).reduce((a, l) => a.concat(l), []);
     }
 
     function factorsOf(n) {
@@ -38,12 +38,13 @@
     function findDuration(delta) {
         if (!durations) initDurations();
         for (const d of durations)
-            if (moment.duration(d).valueOf() > delta / 2) return d;
+            if (moment.duration(d).valueOf() > delta / 2) 
+                return {unit: Object.keys(d)[0], multiple: d[Object.keys(d)[0]]};
     }
 
     function getStartTick(time, unit, multiple) {
         let start = time.clone().startOf(unit);
-        nextMultiple(start, unit, multiple, -1);
+        nextMultiple(start, unit, multiple, step=-1);
         return start;
     }
 
@@ -66,6 +67,22 @@
         console.log(`tick length: ${ticks.length}`)
     }
 
+    function generateTicks(min, max, {unit, multiple}, tolerance) {
+        let start = getStartTick(min, unit, multiple);
+        let ticks = [start.clone()];
+        for (let t = ticks[0]; t.isSameOrBefore(max);) {
+            log(multiple, unit, t, min, start, ticks);
+            nextMultiple(t, unit, multiple);
+
+            let space = t.diff(ticks[ticks.length - 1], unit);
+            if (space < multiple * tolerance) 
+                ticks.pop();
+
+            ticks.push(t.clone())
+        }
+        return ticks.map(t => {return t.valueOf()});
+    }
+
     function init(plot) {
         plot.hooks.processOptions.push(function (plot, options) {
             $.each(plot.getAxes(), function (axisName, axis) {
@@ -75,39 +92,12 @@
                     return;
 
                 axis.tickGenerator = function (axis) {
-                    let min = moment.tz(axis.min, opts.timezone);
-                    let max = moment.tz(axis.max, opts.timezone);
-                    let delta = Math.floor(axis.delta);
-                    let duration = findDuration(delta);
-
-                    let unit = Object.keys(duration)[0];
-                    let multiple = duration[unit];
-
-                    axis.tickSize = [multiple, unit];
-
-                    // console.log(unit)
-                    // console.log(multiple)
-                    let start = getStartTick(min, unit, multiple);
-                    let ticks = [start.clone()];
-
-                    for (let t = ticks[0]; t.isSameOrBefore(max);) {
-                        // rotate through multiples, setting the unit value of t
-                        // each iteration we get a new multiples array, last value will cause t to bubble up to the next higher unit
-                        // (example: unit = hours, multiples = [0, 12, 24], we set hour to 0, then 12, then 24, which causes t to advance to the next day, then repeat)
-                        log(multiple, unit, t, min, start, ticks);
-
-                        nextMultiple(t, unit, multiple);
-
-                        let space = t.diff(ticks[ticks.length - 1], unit);
-
-                        if (space < multiple * opts.tolerance)
-                            ticks.pop();
-
-                        ticks.push(t.clone())
-                    }
-                    return ticks.map(t => {
-                        return t.valueOf()
-                    });
+                    return generateTicks(
+                              min = moment.tz(axis.min, opts.timezone),
+                              max = moment.tz(axis.max, opts.timezone),
+                         duration = findDuration(Math.floor(axis.delta)),
+                        tolerance = opts.tolerance
+                    );
                 };
 
                 axis.tickFormatter = function (v, axis) {
