26e8520039b35beb3e2a103290a279facaee9036
colinhunt <colin.hunt@live.com>
2016-12-13 23:24:53 -0700

    More refactoring and simplification.

--- jquery.flot.temporal.js
+++ jquery.flot.temporal.js
@@ -10,8 +10,6 @@
         }
     };
 
-    const units = ["seconds", "minutes", "hours", "days", "months", "years"];
-    let durations;
 
     function init(plot) {
         plot.hooks.processOptions.push(function (plot, options) {
@@ -21,14 +19,7 @@
                 if (opts.mode != "temporal")
                     return;
 
-                axis.tickGenerator = function (axis) {
-                    return generateTicks(
-                              min = moment.tz(axis.min, opts.timezone),
-                              max = moment.tz(axis.max, opts.timezone),
-                         duration = findDuration(Math.floor(axis.delta)),
-                        tolerance = opts.tolerance
-                    );
-                };
+                axis.tickGenerator = tickGenerator;
 
                 axis.tickFormatter = function (v, axis) {
 
@@ -37,78 +28,85 @@
         });
     }
 
-    function generateTicks(min, max, {unit, multiple}, tolerance) {
-        const start = getStartTick(min, unit, multiple);
-        let ticks = [start];
+    const units = ["seconds", "minutes", "hours", "days", "months", "years"];
+    let durations;
+
+    function tickGenerator(axis) {
+        const min              = moment.tz(axis.min, axis.options.timezone);
+        const max              = moment.tz(axis.max, axis.options.timezone);
+        const [unit, multiple] = findDuration(Math.floor(axis.delta));
+        const tolerance        = axis.options.tolerance;
+        const start            = setToNextMultiple(min.clone().startOf(unit), step=-1);
+
+        let ticks = [start.clone()];
+
         for (let tick = start.clone(); tick.isSameOrBefore(max);) {
-            log(multiple, unit, tick, min, start, ticks);
 
-            setToNextMultiple(tick, unit, multiple);
+            setToNextMultiple(tick);
 
-            addTick(ticks, tick, unit, multiple, tolerance)
+            addTick(tick)
         }
+        // log(ticks[ticks.length - 1]);
         return ticks.map(t => {return t.valueOf()});
-    }
+        
 
-    function getStartTick(tick, unit, multiple) {
-        let start = tick.clone().startOf(unit);
-        setToNextMultiple(start, unit, multiple, step=-1);
-        return start;
-    }
-
-    function setToNextMultiple(tick, unit, multiple, step=1) {
-        do { tick.add(step, unit); } while (getUnit(tick, unit) % multiple);
-    }
-
-    function addTick(ticks, tick, unit, multiple, tolerance) {
-        const space = tick.diff(ticks[ticks.length - 1], unit);
-        if (space < multiple * tolerance) {
-            if (unit == "hours")
-                return;   // clobber this tick
-            ticks.pop();  // clobber the last tick
+        /* Private utility functions */
+        function setToNextMultiple(tick, step=1) {
+            do { tick.add(step, unit); } while (getUnit(tick) % multiple);
+            return tick;
         }
-        ticks.push(tick.clone());
+    
+        function addTick(tick) {
+            const space = tick.diff(ticks[ticks.length - 1], unit);
+            if (space < multiple * tolerance) {
+                if (unit == "hours")
+                    return;   // clobber this tick
+                ticks.pop();  // clobber the last tick
+            }
+            ticks.push(tick.clone());
+        }
+
+        function getUnit(tick) {
+            return unit == "days" ?
+                tick.date() - 1 :
+                tick.get(unit)
+        }
+
+        function findDuration(delta) {
+            if (!durations) initDurations();
+
+            return durations.find(([unit, multiple]) => {
+                return moment.duration(multiple, unit).valueOf() > delta / 2
+            });
+        }
+
+        function initDurations() {
+            durations = units.map((u, i) => {
+                return factorsOf(maxValueOfUnit(i)).map(f => {return [u, f]})
+            }).reduce((a, l) => a.concat(l), []);
+        }
+
+        function factorsOf(n) {
+            return [...Array(n).keys()].filter(i => {return n % i == 0});
+        }
+
+        function maxValueOfUnit(i) {
+            return units[i + 1] ?
+                moment.duration(1, units[i + 1]).as(units[i]) :
+                100000;
+        }
+
+        function log(t) {
+            console.log(`multiple: ${multiple}`);
+            console.log(`unit: ${unit}`);
+            console.log(`min: ${min.format()}`);
+            console.log(`start: ${start.format()}`);
+            console.log(`t: ${t.format()}`);
+            console.log(`tick length: ${ticks.length}`)
+        }
     }
 
-    function getUnit(tick, unit) {
-        if (unit == "days")
-            return tick.date() - 1;
-        return tick.get(unit)
-    }
 
-    function findDuration(delta) {
-        if (!durations) initDurations();
-        for (const d of durations)
-            if (moment.duration(d).valueOf() > delta / 2) 
-                return {unit: Object.keys(d)[0], multiple: d[Object.keys(d)[0]]};
-    }
-
-    function initDurations() {
-        durations = units.map((u, i) => {
-            return factorsOf(maxValueOfUnit(i)).map(f => {
-                return {[u]: f}
-            })
-        }).reduce((a, l) => a.concat(l), []);
-    }
-
-    function factorsOf(n) {
-        return [...Array(n).keys()].filter(i => {return n % i == 0});
-    }
-
-    function maxValueOfUnit(i) {
-        if (units[i + 1])
-            return moment.duration(1, units[i + 1]).as(units[i]);
-        return 100000;
-    }
-
-    function log(multiple, unit, t, min, start, ticks) {
-        console.log(`multiple: ${multiple}`);
-        console.log(`unit: ${unit}`);
-        console.log(`min: ${min.format()}`);
-        console.log(`start: ${start.format()}`);
-        console.log(`t: ${t.format()}`);
-        console.log(`tick length: ${ticks.length}`)
-    }
 
     $.plot.plugins.push({
         init: init,
